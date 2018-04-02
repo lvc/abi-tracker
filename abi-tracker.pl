5b88fe3a30a600a9b3111359c1a29bcbfb5bbb05
Pau Espin Pedrol <pespin@sysmocom.de>
2018-04-03 01:53:39 +0200

    abi-tracker: Allow definining Date from profile version
    This way Date can be generated in the report if the Scm is not supported
    or if a specific Date is wanted instead of the one provided by Scm.

--- abi-tracker.pl
+++ abi-tracker.pl
@@ -1576,8 +1576,12 @@ sub detectDate($)
     
     my $Source = $Profile->{"Versions"}{$V}{"Source"};
     my $Date = undef;
-    
-    if($V eq "current")
+
+    if (defined $Profile->{"Versions"}{$V}{"Date"})
+    {
+        $Date = $Profile->{"Versions"}{$V}{"Date"}
+    }
+    elsif($V eq "current")
     {
         if(defined $Profile->{"Git"})
         {
